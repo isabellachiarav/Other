19af37f827fc267b400deca47e383d53fe8b7c0b
Isabella Chiaravalloti <55116527+isabellachiarav@users.noreply.github.com>
2019-10-28 19:01:07 -0400

    Add files via upload

--- E51.java
+++ E51.java
@@ -7,17 +7,17 @@ public class E51 {
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
-		int zero = 0;
+		final int ZERO = 0;
 		
 		//Asks user to enter an integer
 		System.out.print("Enter an integer: ");
 		int integer = in.nextInt();
 		
 		//Prints if the integer is 0, positive, or negative
-		if (integer == zero) {
-			System.out.println("The integer is " + zero + ".");
+		if (integer == ZERO) {
+			System.out.println("The integer is " + ZERO + ".");
 		}
-		else if (integer > zero) {
+		else if (integer > ZERO) {
 			System.out.println("The integer is positive.");
 		}
 		else {
--- E52.java
+++ E52.java
@@ -7,28 +7,31 @@ public class E52 {
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
+		final int ZERO = 0;
 		
-		//Asks the user to enter an integer
+		//Asks user to enter an integer
 		System.out.print("Enter an integer: ");
 		int integer = in.nextInt();
 		
-		
 		//Prints if the integer is 0, positive, or negative
-		if (integer == 0) {
-			System.out.println("The integer is 0.");
+		if (integer == ZERO) {
+			System.out.println("The integer is " + ZERO + ".");
 		}
-		else if (integer > 0) {
+		else if (integer > ZERO) {
 			System.out.println("The integer is positive.");
 		}
 		else {
 			System.out.println("The integer is negative.");
 		}
 		
+		final int SMALL_THRESHOLD = 1;
+		final int LARGE_THRESHOLD = 1000000;
+		
 		//Prints if the integer is small or large
-		if (Math.abs(integer) < 1) {
+		if (Math.abs(integer) < SMALL_THRESHOLD) {
 			System.out.println("The integer is small.");
 		}
-		else if (Math.abs(integer) > 1000000) {
+		else if (Math.abs(integer) > LARGE_THRESHOLD) {
 			System.out.println("The integer is large.");
 		}
 	}

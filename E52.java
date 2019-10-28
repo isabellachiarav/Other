527da1a3d2be8071ee431fb2e22fffa6616909b5
Isabella Chiaravalloti <55116527+isabellachiarav@users.noreply.github.com>
2019-10-28 18:56:07 -0400

    Add files via upload

--- E52.java
+++ E52.java
@@ -2,15 +2,16 @@ package hw3;
 
 import java.util.Scanner;
 
-public class E51 {
+public class E52 {
 
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
 		
-		//Asks user to enter an integer
+		//Asks the user to enter an integer
 		System.out.print("Enter an integer: ");
 		int integer = in.nextInt();
+		
 		
 		//Prints if the integer is 0, positive, or negative
 		if (integer == 0) {
@@ -23,6 +24,13 @@ public class E51 {
 			System.out.println("The integer is negative.");
 		}
 		
+		//Prints if the integer is small or large
+		if (Math.abs(integer) < 1) {
+			System.out.println("The integer is small.");
+		}
+		else if (Math.abs(integer) > 1000000) {
+			System.out.println("The integer is large.");
+		}
 	}
 
 }

43a79dcee3a69c81b373e8dfe673444792373768
Isabella Chiaravalloti <55116527+isabellachiarav@users.noreply.github.com>
2019-10-28 18:59:45 -0400

    Add files via upload

--- E51.java
+++ E51.java
@@ -7,16 +7,17 @@ public class E51 {
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
+		int zero = 0;
 		
 		//Asks user to enter an integer
 		System.out.print("Enter an integer: ");
 		int integer = in.nextInt();
 		
 		//Prints if the integer is 0, positive, or negative
-		if (integer == 0) {
-			System.out.println("The integer is 0.");
+		if (integer == zero) {
+			System.out.println("The integer is " + zero + ".");
 		}
-		else if (integer > 0) {
+		else if (integer > zero) {
 			System.out.println("The integer is positive.");
 		}
 		else {
